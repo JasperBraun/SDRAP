6d12174cb8bf8da6824cf682e915dcdbc548f62f
JasperBraun <jabbasaccount@hotmail.de>
2019-09-12 10:51:14 -0400

    added tmp directory creation to install.sh

--- install.sh
+++ install.sh
@@ -28,6 +28,7 @@ elif [[ $# -gt 0 ]]; then
 # if no errors detected, make the necessary modifications
 else
   mkdir annotations
+  mkdir tmp
   chown -R $php_username input
   chown -R $php_username annotations
   chown -R $php_username php
